6b9b4c997684aa573f5f8be3ac2222143c4d9d54
1Zuwi1 <zyszkiewicz.lukasz@gmail.com>
2025-12-22 20:51:36 +0100

    initial commit

---
+++ frontend/apps/web/app/page.tsx
@@ -0,0 +1,11 @@
+import { Card, CardHeader } from "@/components/ui/card";
+
+export default function Page() {
+  return (
+    <div>
+      <Card>
+        <CardHeader>Test</CardHeader>
+      </Card>
+    </div>
+  );
+}
