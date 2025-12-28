df05d5664bca6d7155a8889de419b926a7c3b115
1Zuwi1 <zyszkiewicz.lukasz@gmail.com>
2025-12-28 21:22:16 +0100

    Update getUrl function to use dynamic PORT from environment variables and add example environment file

--- frontend/src/lib/get-url.ts
+++ frontend/src/lib/get-url.ts
@@ -4,12 +4,12 @@ import type { NextRequest } from "next/server"
 export async function getUrl(data: NextRequest | Headers): Promise<URL> {
   const h: Headers = data instanceof Headers ? data : await headers()
   const baseUrl: string =
-    data instanceof Headers ? "http://localhost:3001" : data.url
+    data instanceof Headers ? `http://localhost:${process.env.PORT}` : data.url
 
   const proto = h.get("x-forwarded-proto") ?? "http"
   const host = h.get("x-forwarded-host") ?? h.get("host") ?? "localhost"
-  const port = h.get("x-forwarded-port") ?? h.get("port") ?? "3001"
-
+  const port =
+    h.get("x-forwarded-port") ?? h.get("port") ?? `${process.env.PORT}`
   const url = new URL(baseUrl)
   url.protocol = proto
   url.host = host
