5f7ea915ba8b639035ad78a7b24d7b0c967c4ff2
dawid-stolarczyk <dawsto00@gmail.com>
2025-12-30 01:01:39 +0100

    Add unit tests for application context and include JavaMailSender mock

--- backend/src/test/java/com/github/dawid_stolarczyk/magazyn/MagazynApplicationContextTests.java
+++ backend/src/test/java/com/github/dawid_stolarczyk/magazyn/MagazynApplicationContextTests.java
@@ -1,11 +1,23 @@
 package com.github.dawid_stolarczyk.magazyn;
 
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
+import org.mockito.Mock;
+import org.mockito.MockitoAnnotations;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.mail.javamail.JavaMailSender;
+
 
 @SpringBootTest
 class MagazynApplicationContextTests {
+	@Mock
+	private JavaMailSender javaMailSender;
 
+
+	@BeforeEach
+	void setUp() {
+		MockitoAnnotations.openMocks(this);
+	}
 	@Test
 	void contextLoads() {
 	}
