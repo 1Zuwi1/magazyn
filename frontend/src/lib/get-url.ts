5d8a5ca633c0192bc42fd4c2fd85ce217597948e
1Zuwi1 <zyszkiewicz.lukasz@gmail.com>
2026-01-05 18:52:16 +0100

    Refactor getUrl function to use PORT constant for improved readability and maintainability

--- frontend/src/lib/get-url.ts
+++ frontend/src/lib/get-url.ts
@@ -1,15 +1,17 @@
 import { headers } from "next/headers"
 import type { NextRequest } from "next/server"
 
+const DEFAULT_PORT = 3001
+const PORT = process.env.PORT ?? DEFAULT_PORT
+
 export async function getUrl(data: NextRequest | Headers): Promise<URL> {
   const h: Headers = data instanceof Headers ? data : await headers()
   const baseUrl: string =
-    data instanceof Headers ? `http://localhost:${process.env.PORT}` : data.url
+    data instanceof Headers ? `http://localhost:${PORT}` : data.url
 
   const proto = h.get("x-forwarded-proto") ?? "http"
   const host = h.get("x-forwarded-host") ?? h.get("host") ?? "localhost"
-  const port =
-    h.get("x-forwarded-port") ?? h.get("port") ?? `${process.env.PORT}`
+  const port = h.get("x-forwarded-port") ?? h.get("port") ?? PORT.toString()
   const url = new URL(baseUrl)
   url.protocol = proto
   url.host = host
