da2ecfb3bf570a9c83037c641ef3de12923f92cb
Rayzerrek <rayserrek@gmail.com>
2026-01-11 01:25:03 +0100

    add admin panel layout

---
+++ frontend/src/app/(requireAuth)/dashboard/admin/layout.tsx
@@ -0,0 +1,14 @@
+import type React from "react"
+import ProtectedPage from "../../protected-page"
+
+export default function AdminLayout({
+  children,
+}: {
+  children: React.ReactNode
+}) {
+  return (
+    <ProtectedPage needAdminPrivileges>
+      <div className="flex flex-col gap-4 p-4 pt-6">{children}</div>
+    </ProtectedPage>
+  )
+}
