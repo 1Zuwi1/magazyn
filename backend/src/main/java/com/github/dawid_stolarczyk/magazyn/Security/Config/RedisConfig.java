88dbbd940847835a187c590a081011faf51bd52f
dawid-stolarczyk <dawsto00@gmail.com>
2026-01-14 19:48:03 +0100

    Add JsonRedisSerializer and update RedisTemplate to use it for value serialization

--- backend/src/main/java/com/github/dawid_stolarczyk/magazyn/Security/Config/RedisConfig.java
+++ backend/src/main/java/com/github/dawid_stolarczyk/magazyn/Security/Config/RedisConfig.java
@@ -1,5 +1,11 @@
 package com.github.dawid_stolarczyk.magazyn.Security.Config;
 
+import com.fasterxml.jackson.annotation.JsonTypeInfo;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.fasterxml.jackson.databind.json.JsonMapper;
+import com.fasterxml.jackson.databind.jsontype.impl.LaissezFaireSubTypeValidator;
+import com.fasterxml.jackson.datatype.jsr310.JavaTimeModule;
+import com.github.dawid_stolarczyk.magazyn.Security.Auth.JsonRedisSerializer;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.data.redis.connection.RedisConnectionFactory;
@@ -10,16 +16,37 @@ import org.springframework.data.redis.serializer.StringRedisSerializer;
 public class RedisConfig {
 
     @Bean
-    public RedisTemplate<String, Object> redisTemplate(RedisConnectionFactory connectionFactory) {
+    public RedisTemplate<String, Object> redisTemplate(
+            RedisConnectionFactory connectionFactory,
+            ObjectMapper redisObjectMapper
+    ) {
         RedisTemplate<String, Object> template = new RedisTemplate<>();
         template.setConnectionFactory(connectionFactory);
 
-        template.setKeySerializer(new StringRedisSerializer());
-        template.setValueSerializer(new StringRedisSerializer());
-        template.setHashKeySerializer(new StringRedisSerializer());
-        template.setHashValueSerializer(new StringRedisSerializer());
+        StringRedisSerializer stringSerializer = new StringRedisSerializer();
+
+        template.setKeySerializer(stringSerializer);
+        template.setHashKeySerializer(stringSerializer);
+
+        template.setValueSerializer(new JsonRedisSerializer<>(Object.class, redisObjectMapper));
+        template.setHashValueSerializer(new JsonRedisSerializer<>(Object.class, redisObjectMapper));
 
         template.afterPropertiesSet();
         return template;
     }
+    @Bean
+    public ObjectMapper redisObjectMapper() {
+        ObjectMapper mapper = JsonMapper.builder()
+                .addModule(new JavaTimeModule())
+                .build();
+
+        mapper.activateDefaultTyping(
+                LaissezFaireSubTypeValidator.instance,
+                ObjectMapper.DefaultTyping.NON_FINAL,
+                JsonTypeInfo.As.PROPERTY
+        );
+
+        return mapper;
+    }
+
 }
