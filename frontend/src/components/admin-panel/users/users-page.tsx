4ba873d0642bd618c78ca35b895a60674c322801
Rayzerrek <rayserrek@gmail.com>
2026-01-31 23:23:45 +0100

    refactor: fixed empty placeholder

--- frontend/src/components/admin-panel/users/users-page.tsx
+++ frontend/src/components/admin-panel/users/users-page.tsx
@@ -65,7 +65,6 @@ export default function UsersMain() {
         data={users}
         onDelete={handleDeleteUser}
         onEdit={handleEditUser}
-        search=""
       />
 
       <ActionDialog
