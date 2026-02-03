c8af4044e48aef364b184c248a6cab11efd9a27e
dawid-stolarczyk <dawsto00@gmail.com>
2026-02-03 23:28:57 +0100

    feat: simplify Optional import in AssortmentRepository for findByBarcode method

--- backend/src/main/java/com/github/dawid_stolarczyk/magazyn/Repositories/AssortmentRepository.java
+++ backend/src/main/java/com/github/dawid_stolarczyk/magazyn/Repositories/AssortmentRepository.java
@@ -4,11 +4,12 @@ import com.github.dawid_stolarczyk.magazyn.Model.Entity.Assortment;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import java.util.List;
+import java.util.Optional;
 
 public interface AssortmentRepository extends JpaRepository<Assortment, Long> {
     List<Assortment> findByRackId(Long rackId);
 
     boolean existsByBarcode(String barcode);
 
-    java.util.Optional<Assortment> findByBarcode(String barcode);
+    Optional<Assortment> findByBarcode(String barcode);
 }
