8cf636c49d68d4bfdc8c3fad61315868d545b48a
Rayzerrek <rayserrek@gmail.com>
2026-02-07 00:51:38 +0100

    fix:removed  mock session user

--- frontend/src/lib/session.ts
+++ frontend/src/lib/session.ts
@@ -5,13 +5,6 @@ import { apiFetch } from "./fetcher"
 import { ApiMeSchema } from "./schemas"
 
 export const getSession = cache(async (redirectTo?: string) => {
-  if (process.env.NODE_ENV === "development") {
-    return {
-      id: 1,
-      role: "ADMIN",
-      account_status: "ACTIVE",
-    }
-  }
   try {
     const res = await apiFetch("/api/users/me", ApiMeSchema, {
       method: "GET",
