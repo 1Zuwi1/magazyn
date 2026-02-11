27273dc2ef8f2817cc981439ee111d06cd30ecc3
dawid-stolarczyk <dawsto00@gmail.com>
2026-02-11 21:41:59 +0100

    update mail and S3 configuration; enable untrusted attestation for WebAuthn in production properties

--- backend/src/main/java/com/github/dawid_stolarczyk/magazyn/Security/Config/WebAuthnConfig.java
+++ backend/src/main/java/com/github/dawid_stolarczyk/magazyn/Security/Config/WebAuthnConfig.java
@@ -38,7 +38,7 @@ public class WebAuthnConfig {
                 )
                 .credentialRepository(credentialRepository)
                 .origins(Set.of(appUrl))
-                .allowUntrustedAttestation(allowUntrustedAttestation)        // dev/test, produkcja: false
+                .allowUntrustedAttestation(allowUntrustedAttestation)
                 .build();
     }
 }
